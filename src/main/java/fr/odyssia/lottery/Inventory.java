f8d1ef07a0f85feda5e99f9d00e272575fefab79
PieTech <deglonm@gmail.com>
2020-05-15 14:14:02 +0200

    Initial commit

--- src/main/java/fr/odyssia/lottery/Inventory.java
+++ src/main/java/fr/odyssia/lottery/Inventory.java
@@ -1,6 +1,25 @@
 package fr.odyssia.lottery;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 public class Inventory implements Listener {
+
+    @EventHandler
+    public void onClicOnVillager(PlayerInteractEntityEvent e) {
+
+        if (e.getRightClicked().getType()== EntityType.VILLAGER && e.getRightClicked().getCustomName()== "Lotterie"){
+        e.setCancelled(true);
+
+
+        }
+
+    }
+
 }
+
--- src/main/java/fr/odyssia/lottery/LotteryCommand.java
+++ src/main/java/fr/odyssia/lottery/LotteryCommand.java
@@ -1,13 +1,31 @@
 package fr.odyssia.lottery;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Villager;
+import org.bukkit.event.EventHandler;
 
 public class LotteryCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        return false;
-    }
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+            Location loc = p.getLocation();
+            Villager villager = (Villager) p.getWorld().spawnEntity(loc , EntityType.VILLAGER);
+            villager.setRotation(loc.getYaw(), loc.getPitch());
+            villager.setInvulnerable(true);
+            villager.setCustomName(ChatColor.YELLOW+"Lotterie");
+            villager.setCustomNameVisible(true);
+
+        }
+            return false;
+        }
+
+
 }
