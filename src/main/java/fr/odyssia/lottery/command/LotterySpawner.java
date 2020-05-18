40923cc605a478357ce729ab5c36abea4f0ae3b3
PieTech <deglonm@gmail.com>
2020-05-18 16:39:01 +0200

    spawn enderchest near pnj with lottery command

--- src/main/java/fr/odyssia/lottery/command/LotterySpawner.java
+++ src/main/java/fr/odyssia/lottery/command/LotterySpawner.java
@@ -3,6 +3,7 @@ package fr.odyssia.lottery.command;
 import fr.odyssia.lottery.util.Constants;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -17,14 +18,24 @@ public class LotterySpawner implements CommandExecutor {
         if (sender instanceof Player) {
             Player p = (Player) sender;
             Location loc = p.getLocation();
+
             Villager villager = (Villager) p.getWorld().spawnEntity(loc , EntityType.VILLAGER);
             villager.setRotation(loc.getYaw(), loc.getPitch());
             villager.setInvulnerable(true);
             villager.setCustomName(ChatColor.YELLOW + Constants.LOTTERY_VILLAGER_NAME);
             villager.setCustomNameVisible(true);
+            Location blockloc = p.getLocation().add(1,0,0);
+            if (!blockloc.getBlock().getType().equals(Material.AIR)) {
+                blockloc.getBlock().setType(Material.ENDER_CHEST);
+            } else {
+                p.sendMessage("y a une phrase qui dit qu'il y a un bloc  qui gene"); // mettre une phrase si il n'y a pas de de bloc d'air
+                return false;
+            }
+
+
         }
-            return false;
-        }
+        return false;
+    }
 
 
 }
